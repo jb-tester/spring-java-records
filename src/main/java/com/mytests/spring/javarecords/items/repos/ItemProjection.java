4b76637867ca4396a33270e7549c44afb9c4d8bd
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-10-15 10:21:07 +0300

    temporary

---
+++ src/main/java/com/mytests/spring/javarecords/items/repos/ItemProjection.java
@@ -0,0 +1,10 @@
+package com.mytests.spring.javarecords.items.repos;
+
+/**
+ * *
+ * <p>Created by irina on 14.10.2021.</p>
+ * <p>Project: spring-java-records</p>
+ * *
+ */
+public record ItemProjection(String title, String cathegory, int summa) {
+}
---
+++ src/main/java/com/mytests/spring/javarecords/items/repos/ItemsRepository.java
@@ -0,0 +1,22 @@
+package com.mytests.spring.javarecords.items.repos;
+
+import com.mytests.spring.javarecords.items.model.Item;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * *
+ * <p>Created by irina on 14.10.2021.</p>
+ * <p>Project: spring-java-records</p>
+ * *
+ */
+@RepositoryDefinition(domainClass = Item.class, idClass = Integer.class)
+public interface ItemsRepository {
+    @Query("select new com.mytests.spring.javarecords.items.repos.ItemProjection(item.title, item.cathegory, (item.price * (100 - item.discount) * item.total)) from Item item where item.cathegory= :cat")
+    List<ItemProjection> getItemsByCategory(@Param("cat") String category);
+
+    <T> T findProjectedById(Integer id, Class<T> projection);
+}
