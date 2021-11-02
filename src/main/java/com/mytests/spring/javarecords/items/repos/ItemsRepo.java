9718462f48aba1de1650036b1f5f452df0e64bb4
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2021-11-02 16:04:46 +0300

    test text blocks

---
+++ src/main/java/com/mytests/spring/javarecords/items/repos/ItemsRepo.java
@@ -0,0 +1,29 @@
+package com.mytests.spring.javarecords.items.repos;
+
+import com.mytests.spring.javarecords.items.model.Item;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * *
+ * <p>Created by irina on 11/2/2021.</p>
+ * <p>Project: spring-java-records</p>
+ * *
+ */
+public interface ItemsRepo extends CrudRepository<Item, Integer> {
+
+    @Query("""
+           select i from Item i
+           where i.cathegory = :cat
+           and i.price < :price
+           and i.discount > 0
+           """)
+    List<Item> findByMultipleCriteria(String cat, int price);
+
+    @Query("""
+ select i from Item i
+           """)
+    List<Item> allItems();
+}
