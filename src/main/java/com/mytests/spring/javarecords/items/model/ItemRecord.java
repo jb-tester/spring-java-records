fdac34eab8b91c49121eaac84b9dc06383d908d8
Irina.Petrovskaya.295 <irina.petrovskaya@jetbrains.com>
2021-11-03 13:34:30 +0300

    minor

---
+++ src/main/java/com/mytests/spring/javarecords/items/model/ItemRecord.java
@@ -0,0 +1,10 @@
+package com.mytests.spring.javarecords.items.model;
+
+/**
+ * *
+ * <p>Created by irina on 10/15/2021.</p>
+ * <p>Project: spring-java-records</p>
+ * *
+ */
+public record ItemRecord(String aaa) {
+}
